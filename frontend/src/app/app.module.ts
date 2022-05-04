38ad3e1d09709e98f1e3111c032c66481c57c691
adamg <adam.grzenda@gmail.com>
2022-05-05 00:50:20 +0200

    add simple registration component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainFeedComponent } from './main-feed/main-feed.component';
-import { TextCardComponent } from './text-card/text-card.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MainFeedComponent} from './main-feed/main-feed.component';
+import {TextCardComponent} from './text-card/text-card.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {MatButtonModule} from "@angular/material/button";
+import {LoginComponent} from './login/login.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatDividerModule} from "@angular/material/divider";
 
 @NgModule({
   declarations: [
     AppComponent,
     MainFeedComponent,
-    TextCardComponent
+    TextCardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -24,9 +30,14 @@ import {MatButtonModule} from "@angular/material/button";
     MatGridListModule,
     MatIconModule,
     MatButtonToggleModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, ValidationErrors, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  registrationForm = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      passwordRepeated: ['', [Validators.required, Validators.minLength(8)]]
+    }, {validator: this.passwordMatching}
+  );
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.warn("not implemented: " + this.registrationForm.value)
+    console.log(this.registrationForm.get("password"))
+  }
+
+  passwordMatching(control: AbstractControl): ValidationErrors | null {
+    const password = control.get("password")?.value;
+    const passwordRepeated = control.get("passwordRepeated")?.value;
+
+
+    if (password != passwordRepeated) {
+      return {'noMatch': true};
+    } else
+      return null;
+  }
+}
