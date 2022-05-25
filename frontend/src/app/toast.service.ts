5745feaf31b9dcd82ac5019d41e1f8345ecdc012
Adam-Grzenda <65490875+Adam-Grzenda@users.noreply.github.com>
2022-05-25 19:28:18 +0200

    add spinner and toastr

---
+++ frontend/src/app/toast.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {ToastrService} from "ngx-toastr";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor(private toastr: ToastrService) {
+  }
+
+
+  showUnexpectedError() {
+    this.toastr.error("Something went wrong!", "Try again later.")
+  }
+}
